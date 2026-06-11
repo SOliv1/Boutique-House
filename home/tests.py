32624a7b7eef600fcf4f9415b51711e1420a0e53
Sara Oliver <47860987+SOliv1@users.noreply.github.com>
2026-06-11 14:56:20 +0100

    Fix Travis coverage and stabilize Jekyll CI

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class HomeViewTests(TestCase):
+    def test_home_page_returns_200(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_uses_correct_template(self):
+        response = self.client.get(reverse("home"))
+        self.assertTemplateUsed(response, "home/index.html")
