3b84332c4959cc8ef1b38b96e9c411e278ea8cde
Sara Oliver <47860987+SOliv1@users.noreply.github.com>
2026-06-11 15:01:43 +0100

    home tests - add

--- home/home/tests.py
+++ home/home/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.test import TestCase
+
+
+class HomePageTests(TestCase):
+
+    def test_home_page_returns_200(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
