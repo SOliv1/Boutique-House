91a42316fc619a3d7a44efc556c124bac09671e7
Sara Oliver <47860987+SOliv1@users.noreply.github.com>
2026-06-13 16:59:22 +0100

    Handle missing home hero table without homepage 500

--- home/views.py
+++ home/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
+from django.db import OperationalError, ProgrammingError
 
 from .models import HomePageHero
 
 
 def index(request):
     """ A view to return the index page """
-    home_hero = HomePageHero.objects.filter(is_active=True).first()
+    try:
+        home_hero = HomePageHero.objects.filter(is_active=True).first()
+    except (OperationalError, ProgrammingError):
+        home_hero = None
     context = {
         'home_hero': home_hero,
     }
