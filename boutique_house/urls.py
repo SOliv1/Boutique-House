1dbf23da97e8e8670a51663b3fccdf9d820adf51
Sara Oliver <47860987+SOliv1@users.noreply.github.com>
2026-07-17 22:23:49 +0100

    Add API endpoint for vintage notes count

--- boutique_house/urls.py
+++ boutique_house/urls.py
@@ -17,6 +17,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.sitemaps.views import sitemap
+from products.views import vintage_notes_count
 
 from .sitemaps import sitemaps
 from .views import robots_txt
@@ -36,4 +37,5 @@ urlpatterns = [
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
+    path("api/vintage-count/", vintage_notes_count),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
